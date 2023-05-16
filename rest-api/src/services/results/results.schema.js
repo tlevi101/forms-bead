f82b9e1a804b1961b757dfe131b570f3c09d9076
Gyozo Horvath <gyozo.horvath@inf.elte.hu>
2023-05-16 18:32:35 +0200

    first commit

---
+++ rest-api/src/services/results/results.schema.js
@@ -0,0 +1,56 @@
+// // For more information about this file see https://dove.feathersjs.com/guides/cli/service.schemas.html
+import { resolve, virtual } from '@feathersjs/schema'
+import { Type, getValidator, querySyntax } from '@feathersjs/typebox'
+import { dataValidator, queryValidator } from '../../validators.js'
+import { surveySchema } from '../surveys/surveys.schema.js'
+
+// Main data model schema
+export const resultSchema = Type.Object(
+  {
+    id: Type.Number(),
+    content: Type.String(),
+    createdAt: Type.Number(),
+    surveyId: Type.Number(),
+    survey: Type.Ref(surveySchema)
+  },
+  { $id: 'Result', additionalProperties: false }
+)
+export const resultValidator = getValidator(resultSchema, dataValidator)
+export const resultResolver = resolve({
+  survey: virtual(async (result, context) => {
+    return context.app.service('surveys').get(result.surveyId)
+  })
+})
+
+export const resultExternalResolver = resolve({})
+
+// Schema for creating new entries
+export const resultDataSchema = Type.Pick(resultSchema, ['content', 'surveyId'], {
+  $id: 'ResultData'
+})
+export const resultDataValidator = getValidator(resultDataSchema, dataValidator)
+export const resultDataResolver = resolve({
+  createdAt: async () => {
+    return Date.now()
+  }
+})
+
+// Schema for updating existing entries
+export const resultPatchSchema = Type.Partial(resultSchema, {
+  $id: 'ResultPatch'
+})
+export const resultPatchValidator = getValidator(resultPatchSchema, dataValidator)
+export const resultPatchResolver = resolve({})
+
+// Schema for allowed query properties
+export const resultQueryProperties = Type.Pick(resultSchema, ['id', 'content', 'createdAt', 'surveyId'])
+export const resultQuerySchema = Type.Intersect(
+  [
+    querySyntax(resultQueryProperties),
+    // Add additional query properties here
+    Type.Object({}, { additionalProperties: false })
+  ],
+  { additionalProperties: false }
+)
+export const resultQueryValidator = getValidator(resultQuerySchema, queryValidator)
+export const resultQueryResolver = resolve({})
